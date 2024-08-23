e666a19f0d97d673c3b9deed3e3319c3f595ab44
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-23 15:10:38 -0500

    Hibernate-implemented

--- src/main/java/com/riwi/RestFul/entities/EventEntity.java
+++ src/main/java/com/riwi/RestFul/entities/EventEntity.java
@@ -1,9 +1,21 @@
 package com.riwi.RestFul.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
-public class EventEntity {
+@Entity
+@Table(name = "event")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
+public class EventEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) //UUID ID'S
     private String id;
     private String name;
     private Date date;
