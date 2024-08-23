1b0b531bf49c0c936024ce3591842c25ab8540d7
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-23 15:06:00 -0500

    create/event-entity

---
+++ src/main/java/com/riwi/RestFul/entities/EventEntity.java
@@ -0,0 +1,12 @@
+package com.riwi.RestFul.entities;
+
+import java.util.Date;
+
+public class EventEntity {
+
+    private String id;
+    private String name;
+    private Date date;
+    private String ubication;
+    private Integer capacity;
+}
