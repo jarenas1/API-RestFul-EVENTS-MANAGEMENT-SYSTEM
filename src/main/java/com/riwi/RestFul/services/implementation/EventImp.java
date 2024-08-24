764439334c58a31198deceeea19319b5b18b3c88
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-23 20:52:07 -0500

    servicesImp

--- src/main/java/com/riwi/RestFul/services/implementation/EventImp.java
+++ src/main/java/com/riwi/RestFul/services/implementation/EventImp.java
@@ -1,4 +1,35 @@
 package com.riwi.RestFul.services.implementation;
 
-public class EventImp {
+import com.riwi.RestFul.entities.EventEntity;
+import com.riwi.RestFul.repositories.EventRepository;
+import com.riwi.RestFul.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class EventImp implements EventService {
+
+    //INYECTAMOS REPO PARA ACCEDER A LOS METODOS
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Override
+    public List<EventEntity> getAll() {
+        return eventRepository.findAll();
+    }
+
+    @Override
+    public EventEntity saveEvent(EventEntity eventEntity) {
+        return eventRepository.save(eventEntity);
+    }
+
+    @Override
+    public EventEntity getEventById(String id) {
+        return eventRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteEvent(String id) {
+        eventRepository.deleteById(id);
+    }
 }
